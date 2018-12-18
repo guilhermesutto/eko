6d057dc12808cfcfd741ffa463d97bf0dc232a38
Guilherme Sutto <guilhermesutto91@gmail.com>
2018-12-18 21:26:19 -0200

    a

--- painel/config/database.php
+++ painel/config/database.php
@@ -41,12 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '172.19.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'eko_alliance'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', '123456'),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'username' => env('DB_USERNAME', 'eko'),
+            'password' => env('DB_PASSWORD', 'webshark'),            
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
