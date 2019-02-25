39476787884731a52ad0ec6c430c99466f5464aa
Guilherme Sutto <guilhermesutto@gmail.com>
2019-02-24 22:16:01 -0300

    a

--- index.php
+++ index.php
@@ -165,7 +165,7 @@ function getProjetos($projetos){
           </div>
           <div class="row row-30">
             <?php 
-                foreach($Projetos as $key=>$value){
+                foreach($Projetos as $key=>$value){ print_r($value); exit;
                   echo '
                     <div class="col-sm-6 col-lg-4 wow-outer">
                       <!-- Box Creative-->
