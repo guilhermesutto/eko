ccb3ec04b8e1848e808e5166a7850999838f9dfa
Guilherme Sutto <guilhermesutto@gmail.com>
2019-02-24 22:13:34 -0300

    a

--- about-us.php
+++ about-us.php
@@ -1,4 +1,10 @@
-<?php include("include/head.php"); ?>
+<?php include("include/head.php"); 
+include("include/db.php");
+
+//Querys
+$data = dbQuerySingle("about_us", "id = 1");
+
+?>
       <!-- Breadcrumbs -->
       <section class="breadcrumbs-custom bg-image context-dark" style="background-image: url(images/imgAboutUs.jpeg);">
         <div class="breadcrumbs-custom-inner">
@@ -8,7 +14,7 @@
               <h1 class="breadcrumbs-custom-title">About Us</h1>
             </div>
             <ul class="breadcrumbs-custom-path">
-              <li><a href="index.html">Home</a></li>
+              <li><a href="index.php">Home</a></li>
               <li><a href="#">Pages</a></li>
               <li class="active">About Us</li>
             </ul>
@@ -20,15 +26,9 @@
         <div class="container">
           <div class="row row-50 justify-content-center justify-content-lg-between">
             <div class="col-md-10 col-lg-6 column-ethereal">
-                <h3>Who We are</h3>
+                <h3><?php echo $data->titulo; ?></h3>
               
-                <p>Eko Alliance is a non-profit organization based in São Paulo, Brazil.
-                    Our goal is to provide na internacional volunteer program.</p>
-                <p>Our programs are developded with partnership with Non-Profits and
-                    Associations that take care of people in situations of high social
-                    vulnerability.</p>
-                <p>The volunteers who want to join our program must choose the option
-                    that fits better according to his/her background.</p>    
+                <?php echo $data->texto; ?>
               
             </div>
             <div class="col-md-10 col-lg-6 col-xl-5 align-self-end"><img class="img-responsive" src="images/about-01-510x482.png" alt="" width="510" height="482"/>
@@ -46,8 +46,7 @@
               <div class="timeline-classic-item-divider"></div>
               <div class="timeline-classic-item-main">
                 <p class="timeline-classic-item-title">Vision</p>                
-                <p>We believe small actions can change the World.</p>
-                <p>Being an Association recognized for stimulating people who seek through the volunteer program to develop actions that can transform the environment. We believe we can change the World with small actions.</p>
+                <?php echo $data->visao; ?>
             </div>
             </div>
             <div class="timeline-classic-item">
@@ -56,7 +55,7 @@
               <div class="timeline-classic-item-divider"></div>
               <div class="timeline-classic-item-main">
                 <p class="timeline-classic-item-title">Mission</p>                
-                <p>Eko Volunteers works to contribute for the development of people in High social vulnerability . For this reason, we made the Voluntary program more acessible, with a low cost in order to more volunteers be able to contribute with changes in those people`s life.</p>
+                <?php echo $data->missao; ?>
               </div>
             </div>
             <div class="timeline-classic-item">
@@ -65,64 +64,14 @@
               <div class="timeline-classic-item-divider"></div>
               <div class="timeline-classic-item-main">
                 <p class="timeline-classic-item-title">Values</p>
-                <p>Respect <br> We respect every religion, gender and ...</p>
-                <p style="color: #f5f6fa">Mauris tincidunt pretium nulla vitae volutpat. Nullam gravida, augue at scelerisque interdum, metus lacus vestibulum justo, ut imperdiet urna magna et lorem. Suspendisse eu purus quis velit consequat vestibulum. Donec placerat sodales mollis.</p>
+                <<?php echo $data->valores; ?>
               </div>
             </div>            
           </article>
         </div>
       </section>
       <!-- Page Footer-->
-            <footer class="section footer-standard bg-gray-modern">
-              <div class="section-wave">
-                <svg x="0px" y="0px" viewBox="0 0 1920 45" width="1920" height="45px" preserveAspectRatio="none">
-                  <style type="text/css"></style>
-                  <path d="M1920,0c-82.8,0-108.8,44.4-192,44.4c-78.8,0-116.5-43.7-192-43.7c-77.1,0-115.9,44.4-192,44.4c-78.2,0-114.6-44.4-192-44.4c-78.4,0-115.3,44.4-192,44.4C883.1,45,841,0.6,768,0.6C691,0.6,652.8,45,576,45C502.4,45,461.9,0.6,385,0.6C306.5,0.6,267.9,45,191,45C115.1,45,78,0.6,0,0.6V45h1920V0z"></path>
-                </svg>
-              </div>
-              <div class="footer-standard-main">
-                <div class="container">
-                  <div class="row row-50">
-                    <div class="col-lg-4">
-                      <div class="inset-right-1">
-                        <h4>About Us</h4>
-                        <p>Water is one of the leading spring water providers in the USA. We bottle and deliver bacteria-free spring water throughout the country covering all water needs and tastes. We are proud to be the nation’s #1 water provider!</p>
-                      </div>
-                    </div>
-                    <div class="col-sm-6 col-md-5 col-lg-4">
-                      <div class="box-1">
-                        <h4>Contact Information</h4>
-                        <ul class="list-sm list-footer">
-                          <li class="object-inline"><span class="icon icon-sm mdi mdi-map-marker text-gray-700"></span><a class="link-default" href="#">2130 Fulton Street <br> San Diego, CA 94117-1080 USA</a></li>
-                          <li class="object-inline"><span class="icon icon-sm mdi mdi-phone text-gray-700"></span><a class="link-default" href="tel:#">1-800-1234-678</a></li>
-                          <li class="object-inline"><span class="icon icon-sm mdi mdi-email text-gray-700"></span><a class="link-default" href="mailto:#">info@demolink.org</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="col-sm-6 col-md-7 col-lg-4">
-                      <h4>Newsletter</h4>
-                      <p>Sign up to our newsletter and be the first to know about latest news, special offers, events, and discounts.</p>
-                      <!-- RD Mailform-->
-                      <form class="rd-form rd-mailform form-inline" data-form-output="form-output-global" data-form-type="subscribe" method="post" action="bat/rd-mailform.php">
-                        <div class="form-wrap">
-                          <input class="form-input" id="subscribe-form-2-email" type="email" name="email" data-constraints="@Email @Required">
-                          <label class="form-label" for="subscribe-form-2-email">E-mail</label>
-                        </div>
-                        <div class="form-button">
-                          <button class="button button-primary button-icon button-icon-only button-winona" type="submit" aria-label="submit"><span class="icon mdi mdi-email-outline"></span></button>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="container">
-                <div class="footer-standard-aside justify-content-center">
-                  <!-- Rights-->
-                  <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>All Rights Reserved.</span><span>&nbsp;</span><br class="d-sm-none"/><a href="#">Terms of Use</a><span> and</span><span>&nbsp;</span><a href="terms-of-use.html">Privacy Policy</a></p>
-                </div>
-              </div>
-            </footer>
+            <?php include("footer.php"); ?>
     </div>
     <div class="preloader">
       <div class="preloader-body">
