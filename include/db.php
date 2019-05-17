75e2accabb6988015cb6bafc7912d382461d53eb
Guilherme Sutto <guilhermesutto@MacBook-Pro-de-Guilherme.local>
2019-05-16 23:22:52 -0300

    a

--- include/db.php
+++ include/db.php
@@ -8,7 +8,7 @@
         $conn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
-        shell_exec("/etc/init.d/mysql restart");
+        exec("/etc/init.d/mysql restart");
         header("Refresh: 0");        
         //echo 'ERROR: ' . $e->getMessage();
     }
