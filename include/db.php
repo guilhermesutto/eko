f809d085a77ebcb10f59c70f5caa12288bf496f2
Guilherme Sutto <guilhermesutto91@gmail.com>
2019-08-12 12:27:38 -0300

    a

--- include/db.php
+++ include/db.php
@@ -5,6 +5,7 @@
     $password = "dbweb";    
 
     try {
+        phpinfo();
         $conn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
